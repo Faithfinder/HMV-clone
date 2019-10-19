eccac55dc65a3d64b1cdf4fd7bd8e1f547b1703c
Faithfinder <faithfinder@yandex.ru>
2019-10-19 18:32:03 -0400

    basic CRUD for items

--- server/src/handlers/items.js
+++ server/src/handlers/items.js
@@ -1,22 +1,48 @@
-import db from "../models";
+import { Item } from "../models";
 
 export const getItems = async (req, res, next) => {
     try {
-        let items = await db.Item.find();
+        const items = await Item.find();
         return res.status(200).json(items);
     } catch (err) {
         console.log(err);
     }
 };
 export const createItem = async (req, res, next) => {
-    return res.send("Create item");
+    try {
+        const item = await Item.create(req.body.item);
+        await item.save();
+        const foundItem = await Item.findById(item._id);
+        return res.status(200).json(foundItem);
+    } catch (err) {
+        console.log(err);
+    }
 };
 export const getItem = async (req, res, next) => {
-    return res.send("Get item");
+    try {
+        const item = await Item.findById(req.params.item_id);
+        return res.status(200).json(item);
+    } catch (err) {
+        console.log(err);
+    }
 };
 export const updateItem = async (req, res, next) => {
-    return res.send("Update item");
+    try {
+        const item = await Item.findByIdAndUpdate(
+            req.params.item_id,
+            req.body.item,
+            { new: true }
+        );
+        return res.status(200).json(item);
+    } catch (err) {
+        console.log(err);
+    }
 };
 export const deleteItem = async (req, res, next) => {
-    return res.send("Delete item");
+    try {
+        await Item.findByIdAndDelete(req.params.item_id);
+        return res.sendStatus(204);
+    } catch (err) {
+        console.log(err);
+    }
 };
