e1eec712b5d8f90a5f0e860b2f613c88dcf57b1c
Faithfinder <faithfinder@yandex.ru>
2019-10-19 19:29:14 -0400

    Basic error handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import bodyParser from "body-parser";
+import errorHandler from "./handlers/error";
 import itemsRoutes from "./routes/items";
 
 app.use(bodyParser.json());
@@ -10,6 +11,14 @@ app.use("/items", itemsRoutes);
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
+
+app.use((req, res, next) => {
+    let err = new Error("Resource not found");
+    err.status = 404;
+    next(err);
+});
+
+app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is starting on port ${process.env.PORT}`);
--- server/src/handlers/items.js
+++ server/src/handlers/items.js
@@ -5,7 +5,7 @@ export const getItems = async (req, res, next) => {
         const items = await Item.find();
         return res.status(200).json(items);
     } catch (err) {
-        console.log(err);
+       next(err);
     }
 };
 export const createItem = async (req, res, next) => {
@@ -15,7 +15,7 @@ export const createItem = async (req, res, next) => {
         const foundItem = await Item.findById(item._id);
         return res.status(200).json(foundItem);
     } catch (err) {
-        console.log(err);
+        next(err);
     }
 };
 export const getItem = async (req, res, next) => {
@@ -23,7 +23,7 @@ export const getItem = async (req, res, next) => {
         const item = await Item.findById(req.params.item_id);
         return res.status(200).json(item);
     } catch (err) {
-        console.log(err);
+        next(err);
     }
 };
 export const updateItem = async (req, res, next) => {
@@ -35,7 +35,7 @@ export const updateItem = async (req, res, next) => {
         );
         return res.status(200).json(item);
     } catch (err) {
-        console.log(err);
+        next(err);
     }
 };
 export const deleteItem = async (req, res, next) => {
@@ -43,6 +43,6 @@ export const deleteItem = async (req, res, next) => {
         await Item.findByIdAndDelete(req.params.item_id);
         return res.sendStatus(204);
     } catch (err) {
-        console.log(err);
+        next(err);
     }
 };
