6fa5069a2cb7838a89e9dfb7176acce38bae2bb8
Faithfinder <faithfinder@yandex.ru>
2019-10-19 12:35:39 -0400

    basic setup

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is starting on port ${process.env.PORT}`);
+});
