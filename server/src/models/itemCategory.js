3053c2dbcf836bf810cb87305735f8fec4051194
Faithfinder <faithfinder@yandex.ru>
2019-10-19 20:18:35 -0400

    make category title unique

--- server/src/models/itemCategory.js
+++ server/src/models/itemCategory.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const itemCategorySchema = new mongoose.Schema({
-    title: { type: String, required: true }
+    title: { type: String, required: true, unique:true }
 });
 
 const ItemCategory = mongoose.model("ItemCategory", itemCategorySchema);
