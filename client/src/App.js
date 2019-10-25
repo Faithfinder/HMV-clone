baa7d44a9fb574e67aa62316d4a3c55f8c9754be
Faithfinder <faithfinder@yandex.ru>
2019-10-25 15:59:55 -0400

    Facebook button

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import io from "socket.io-client";
 import axios from "axios";
-import OAuth from "./OAuth";
+import OAuth from "./components/Facebook";
 
 const socket = io();
 
---
+++ client/src/components/Facebook.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+import Button from "react-bootstrap/Button";
+
+import "./Facebook.css";
+export default ({ socket }) => {
+    const [user, setUser] = useState(undefined);
+    const [, setPopup] = useState(undefined);
+
+    const openPopup = () => {
+        const width = 600,
+            height = 600;
+        const left = window.innerWidth / 2 - width / 2;
+        const top = window.innerHeight / 2 - height / 2;
+        const url = `api/auth/facebook?socketId=${socket.id}`;
+
+        return window.open(
+            url,
+            "",
+            `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+      scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+      height=${height}, top=${top}, left=${left}`
+        );
+    };
+
+    const startAuth = () => {
+        const newPopup = openPopup();
+        setPopup(newPopup);
+        attachOnAuthnticateToSocket(newPopup);
+    };
+
+    const cancelAuth = () => {
+        setUser(undefined);
+    };
+
+    const attachOnAuthnticateToSocket = newPopup => {
+        socket.on("facebook", newUser => {
+            newPopup.close();
+            setUser(newUser);
+        });
+    };
+
+    const getButtonText = user => {
+        if (user) {
+            return (
+                <>
+                    {user.emails[0].value} <FontAwesomeIcon icon={faTimes} />
+                </>
+            );
+        } else {
+            return "Log in with Facebook";
+        }
+    };
+
+    const buttonText = getButtonText(user);
+
+    return (
+        <Button className="facebook" onClick={user ? cancelAuth : startAuth}>
+            <FontAwesomeIcon icon={faFacebookSquare} /> {buttonText}
+        </Button>
+    );
+};
