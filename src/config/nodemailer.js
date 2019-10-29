a1bf62c10ff89e992dc90fa5d0d6bf59b60cfd5a
Faithfinder <faithfinder@yandex.ru>
2019-10-28 21:09:33 -0400

    nodemailer basic config

---
+++ src/config/nodemailer.js
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+
+var transport = nodemailer.createTransport({
+    host: process.env.MAIL_MAIL_SERVER,
+    port: process.env.MAIL_PORT,
+    auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD
+    }
+});
+
+export default transport;
