b4a89ded5bcfb4eddde694e23436b0f0b0129264
Faithfinder <faithfinder@yandex.ru>
2019-10-29 22:52:33 -0400

    typo

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 var transport = nodemailer.createTransport({
-    host: process.env.MAIL_MAIL_SERVER,
+    host: process.env.MAIL_SERVER,
     port: process.env.MAIL_PORT,
     auth: {
         user: process.env.MAIL_USERNAME,
