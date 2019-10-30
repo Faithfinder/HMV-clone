d9752951979ceb39310b4695a7feb1a280cd0f54
Faithfinder <faithfinder@yandex.ru>
2019-10-30 17:43:52 -0400

    discount is not an array

--- src/models/bundle.js
+++ src/models/bundle.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const bundleSchema = new mongoose.Schema({
     title: { type: String, required: true },
     items: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
-    discount: [{ type: Number, default: 0, min: 0, max: 100 }]
+    discount: { type: Number, default: 0, min: 0, max: 100 }
 });
 
 const Bundle = mongoose.model("Bundle", bundleSchema);
