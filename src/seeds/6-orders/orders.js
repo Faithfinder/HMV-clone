f47af3e6bf15cb2cc61c95d9f913d63ea11a5392
Faithfinder <faithfinder@yandex.ru>
2019-10-30 21:38:55 -0400

    Orders seed

---
+++ src/seeds/6-orders/orders.js
@@ -0,0 +1,51 @@
+const faker = require("faker");
+const { getObjectId } = require("../helper");
+
+let reviewedTotal = 0;
+const reviewdItems = [1, 2, 3, 4, 5].map(value => {
+    const price = Number(faker.commerce.price());
+    reviewedTotal += price;
+    return {
+        id: getObjectId("Reviewed" + value),
+        price,
+        title: faker.commerce.productName(),
+        amount: faker.random.number()
+    };
+});
+
+let bundledTotal = 0;
+const bundledItems = [1, 2, 3, 4, 5].map(value => {
+    const price = Number(faker.commerce.price());
+    bundledTotal += price;
+    return {
+        id: getObjectId("Bundled" + value),
+        price,
+        title: faker.commerce.productName(),
+        amount: faker.random.number()
+    };
+});
+
+module.exports = [
+    {
+        number: faker.random.number(),
+        items: reviewdItems,
+        user: {
+            id: getObjectId("User1"),
+            email: faker.internet.email()
+        },
+        total: reviewedTotal,
+        fulfilled: false,
+        labels: []
+    },
+    {
+        number: faker.random.number(),
+        items: bundledItems,
+        user: {
+            id: getObjectId("User1"),
+            email: faker.internet.email()
+        },
+        total: bundledTotal,
+        fulfilled: true,
+        labels: ["tagged"]
+    }
+];
