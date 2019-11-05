0161fe8abec8db937700e04eee7390ee0c55f4e1
Faithfinder <faithfinder@yandex.ru>
2019-11-04 21:22:47 -0500

    Navbar switched to Material UI

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,16 +1,31 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
 import Facebook from "../Facebook";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import ShoppingCartButton from "./ShoppingCartButton";
 
-export default () => {
-    return (
-        <>
-            <Facebook />
+const useStyles = makeStyles(theme => ({
+    title: {
+        flexGrow: 1
+    }
+}));
 
-            <ShoppingCartButton />
-        </>
+export default () => {
+    const classes = useStyles();
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography variant="h6" className={classes.title}>
+                    Media Store Prototype
+                </Typography>
+                <Facebook />
+
+                <ShoppingCartButton />
+            </Toolbar>
+        </AppBar>
     );
 };
