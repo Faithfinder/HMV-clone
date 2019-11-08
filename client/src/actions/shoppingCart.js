a1f9082a47f1ec119d290edac9ef42a90b691cc5
Faithfinder <faithfinder@yandex.ru>
2019-11-07 23:51:05 -0500

    Get item data from server with the cart

--- client/src/actions/shoppingCart.js
+++ client/src/actions/shoppingCart.js
@@ -2,9 +2,12 @@ import {
     CART_ADD_REQUEST,
     CART_ADD_RESPONSE,
     CART_CHECK_REQUEST,
-    CART_CHECK_RESPONSE
+    CART_CHECK_RESPONSE,
+    ITEMS_FETCH_REQUEST,
+    ITEMS_FETCH_RESPONSE
 } from "./types";
 import axios from "axios";
+import { batch } from "react-redux";
 
 export const addToCart = item => async (dispatch, getState) => {
     const updatedCart = putItemIntoCart(getState, item);
@@ -38,21 +41,39 @@ function putItemIntoCart(getState, item) {
 
 export const checkCart = () => async dispatch => {
     let errored = false;
-    let payload;
+    let cartPayload;
+    let itemsPayload;
     try {
-        dispatch({ type: CART_CHECK_REQUEST });
+        batch(() => {
+            dispatch({ type: CART_CHECK_REQUEST });
+            dispatch({ type: ITEMS_FETCH_REQUEST });
+        });
+
         const response = await axios.get("/api/cart");
         if (response.status === 200) {
-            payload = response.data;
+            cartPayload = response.data.cart;
+            itemsPayload = response.data.items;
         } else {
             errored = true;
-            payload = new Error(
+            const error = new Error(
                 `Couldn't get cart from server: ${response.statusText}`
             );
+            cartPayload = error;
+            itemsPayload = error;
+            cartPayload = error;
         }
     } catch (error) {
         errored = true;
-        payload = error;
+        cartPayload = error;
     }
-    dispatch({ type: CART_CHECK_RESPONSE, payload, error: errored });
+    dispatch({
+        type: ITEMS_FETCH_RESPONSE,
+        payload: itemsPayload,
+        error: errored
+    });
+    dispatch({
+        type: CART_CHECK_RESPONSE,
+        payload: cartPayload,
+        error: errored
+    });
 };
--- src/handlers/routes/cart.js
+++ src/handlers/routes/cart.js
@@ -1,7 +1,12 @@
+import { Item } from "../../models";
+
 export const getCart = async ({ session: { cart } }, res, next) => {
     try {
         if (!cart) cart = {};
-        return res.status(200).json(cart);
+        const items = await Item.find({
+            _id: { $in: Object.keys(cart) }
+        });
+        return res.status(200).json({ cart, items });
     } catch (err) {
         next(err);
     }
