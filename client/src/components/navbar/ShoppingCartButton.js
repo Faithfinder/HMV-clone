d75e4a1303e8c607b31f22a133068279d54386ff
Faithfinder <faithfinder@yandex.ru>
2019-11-07 23:01:40 -0500

    remove unnecessary import

--- client/src/components/navbar/ShoppingCartButton.js
+++ client/src/components/navbar/ShoppingCartButton.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import IconButton from "@material-ui/core/IconButton";
