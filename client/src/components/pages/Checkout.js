37debacf45510bf75432f3e04fc7a55ce4139111
Faithfinder <faithfinder@yandex.ru>
2019-11-10 18:11:07 -0500

    List cart on checkout page

--- client/src/components/pages/Checkout.js
+++ client/src/components/pages/Checkout.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import Grid from "@material-ui/core/Grid";
+import { useCartContents } from "src/selectors/shoppingCart";
+import { CartItem } from "src/components/shoppingCart/CartItem";
+import Container from "@material-ui/core/Container";
 
 export default () => {
+    const cartContents = useCartContents();
     return (
-        <div>
-            I am checkout!
-        </div>
-    )
-}
+        <Container>
+            <Grid container direction="column">
+                {cartContents.map(item => (
+                    <CartItem key={item.id} item={item} />
+                ))}
+            </Grid>
+        </Container>
+    );
+};
