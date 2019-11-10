b8c53e7ab7866eaa5e922f49acae1654c021d8c7
Faithfinder <faithfinder@yandex.ru>
2019-11-09 21:08:08 -0500

    private route component

---
+++ client/src/components/auth/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+export default ({ children, ...props }) => {
+    const [isAuthenticated, authRefresh] = useSelector(({ authentication }) => [
+        authentication.user,
+        authentication.refreshing
+    ]);
+
+    if (authRefresh) {
+        return <CircularProgress />;
+    } else if (isAuthenticated) {
+        return <Route {...props}>{children}</Route>;
+    } else {
+        return <Redirect to="/login" />;
+    }
+};
