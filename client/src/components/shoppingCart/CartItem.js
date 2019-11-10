2f2a2108e42ba0faca891ea29352b1249a98709e
Faithfinder <faithfinder@yandex.ru>
2019-11-10 18:57:25 -0500

    Remove unneeded import

--- client/src/components/shoppingCart/CartItem.js
+++ client/src/components/shoppingCart/CartItem.js
@@ -5,7 +5,6 @@ import { useDispatch, useSelector } from "react-redux";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
