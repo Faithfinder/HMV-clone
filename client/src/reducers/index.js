0c9f4deda8892ac92a85b8cd1d4e9cdb548bdeef
Faithfinder <faithfinder@yandex.ru>
2019-11-13 20:21:31 -0500

    Add orders to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,11 +4,13 @@ import socket from "src/reducers/socket";
 import shoppingCart from "src/reducers/shoppingCart";
 import items from "src/reducers/items";
 import ui from "src/reducers/ui";
+import orders from "src/reducers/orders";
 
 export default combineReducers({
     authentication,
     socket,
     shoppingCart,
+    orders,
     items,
     ui
 });
