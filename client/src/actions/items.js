25925323bd52e67ecb237872578b23459d7dfdd4
Faithfinder <faithfinder@yandex.ru>
2019-11-16 02:59:46 -0500

    Improve naming

--- client/src/actions/items.js
+++ client/src/actions/items.js
@@ -1,26 +1,26 @@
 import { createAction } from "redux-actions";
 
-import { items } from "src/types/state/actions";
-import backend from "src/services/backend/items";
+import { items as itemsTypes } from "src/types/state/actions";
+import itemsBackend from "src/services/backend/items";
 
 export const fetchItems = filter => async dispatch => {
     let payload;
-    dispatch({ type: items.fetchRequest });
+    dispatch({ type: itemsTypes.fetchRequest });
     try {
-        payload = await backend.fetchFiltered(filter);
+        payload = await itemsBackend.fetchFiltered(filter);
     } catch (error) {
         payload = error;
     }
-    dispatch(createAction(items.fetchResponse)(payload));
+    dispatch(createAction(itemsTypes.fetchResponse)(payload));
 };
 
 export const fetchFeatured = () => async dispatch => {
     let payload;
     try {
-        dispatch({ type: items.fetchRequest });
-        payload = await backend.fetchFeatured();
+        dispatch({ type: itemsTypes.fetchRequest });
+        payload = await itemsBackend.fetchFeatured();
     } catch (error) {
         payload = error;
     }
-    dispatch(createAction(items.fetchResponse)(payload));
+    dispatch(createAction(itemsTypes.fetchResponse)(payload));
 };
