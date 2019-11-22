a1942bc9f5f423d5da3eebf9934744d0ff58dcb0
Faithfinder <faithfinder@yandex.ru>
2019-11-22 00:05:23 -0500

    Bugs in review schema

--- src/models/review.js
+++ src/models/review.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const reviewSchema = new mongoose.Schema({
     rating: { type: Number, min: 1, max: 5, required: true },
-    author: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    content: [{ type: String, required: true }]
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, required: true }
 });
 
 const Review = mongoose.model("Review", reviewSchema);
