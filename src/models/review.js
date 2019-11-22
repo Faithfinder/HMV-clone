076bbe931c0a8acf4dcea6062af46804d9771c63
Faithfinder <faithfinder@yandex.ru>
2019-11-22 01:23:17 -0500

    Timestamps in review schema

--- src/models/review.js
+++ src/models/review.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema({
-    rating: { type: Number, min: 1, max: 5, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    content: { type: String, required: true }
-});
+const reviewSchema = new mongoose.Schema(
+    {
+        rating: { type: Number, min: 1, max: 5, required: true },
+        author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        content: { type: String, required: true }
+    },
+    { timestamps: true }
+);
 
 const Review = mongoose.model("Review", reviewSchema);
 module.exports = Review;
--- src/seeds/5-reviews/reviews.js
+++ src/seeds/5-reviews/reviews.js
@@ -5,6 +5,7 @@ const reviews1 = [1, 2, 3, 4, 5].map(value => {
     return {
         _id: getObjectId("Review" + value),
         rating: faker.random.number({ min: 1, max: 5 }),
+        createdAt: new Date(),
         author: getObjectId("User" + value),
         content: faker.lorem.paragraphs(5)
     };
@@ -14,6 +15,7 @@ const reviews2 = [10, 20, 30, 40, 50].map(value => {
     return {
         _id: getObjectId("Review" + value),
         rating: faker.random.number({ min: 1, max: 5 }),
+        createdAt: new Date(),
         author: getObjectId("User" + value),
         content: faker.lorem.paragraphs(5)
     };
