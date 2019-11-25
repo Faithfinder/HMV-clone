49f386f1c4b28d795a0ca9313ffb3a1082331230
Faithfinder <faithfinder@yandex.ru>
2019-11-25 14:09:12 -0500

    review title in seeds

--- src/seeds/5-reviews/reviews.js
+++ src/seeds/5-reviews/reviews.js
@@ -5,6 +5,7 @@ const reviews1 = [1, 2, 3, 4, 5].map(value => {
     return {
         _id: getObjectId("Review" + value),
         rating: faker.random.number({ min: 1, max: 5 }),
+        title: faker.random.words(3),
         createdAt: new Date(),
         author: getObjectId("User" + value),
         content: faker.lorem.paragraphs(5)
@@ -15,6 +16,7 @@ const reviews2 = [10, 20, 30, 40, 50].map(value => {
     return {
         _id: getObjectId("Review" + value),
         rating: faker.random.number({ min: 1, max: 5 }),
+        title: faker.random.words(3),
         createdAt: new Date(),
         author: getObjectId("User" + value),
         content: faker.lorem.paragraphs(5)
