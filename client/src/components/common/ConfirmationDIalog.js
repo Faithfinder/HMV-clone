cded8515bd985d36dae0449eca88a00ded1e53cf
Faithfinder <faithfinder@yandex.ru>
2019-11-25 22:24:12 -0500

    Confirmation dialog

---
+++ client/src/components/common/ConfirmationDIalog.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+const ConfirmationDialog = ({
+    title,
+    description,
+    confirmationHandler,
+    open,
+    setOpen
+}) => {
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>{description}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    No
+                </Button>
+                <Button onClick={confirmationHandler} color="primary" autoFocus>
+                    Yes
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default ConfirmationDialog;
