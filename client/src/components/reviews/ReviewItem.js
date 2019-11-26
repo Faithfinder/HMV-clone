66db26f83f2c2be37df4d93d7fff6dfefc7c9ffb
Faithfinder <faithfinder@yandex.ru>
2019-11-25 22:45:29 -0500

    Delete review handling

--- client/src/components/reviews/ReviewItem.js
+++ client/src/components/reviews/ReviewItem.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Rating from "@material-ui/lab/Rating";
@@ -11,7 +13,10 @@ import Button from "@material-ui/core/Button";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
-import PrivateComponent from "../auth/PrivateComponent";
+
+import PrivateComponent from "src/components/auth/PrivateComponent";
+import { deleteReview } from "src/actions/reviews";
+import useConfirmationDialog from "src/components/common/useConfirmationDIalog";
 
 const useStyles = makeStyles(theme => ({
     grow: {
@@ -29,43 +34,61 @@ const ReviewItem = ({ review }) => {
     const classes = useStyles();
     const [expanded, setExpanded] = useState(false);
 
+    const { itemId } = useParams();
+    const dispatch = useDispatch();
+
+    const deleteHandler = () => {
+        dispatch(deleteReview(itemId, review._id));
+    };
+
+    const [openDialog, Dialog] = useConfirmationDialog(
+        "Do you want to delete this review?",
+        "The review will be deleted permanently",
+        deleteHandler
+    );
+
     return (
-        <Grid item component={Paper} elevation={0}>
-            <Grid container alignItems="center">
-                <div>
-                    <Typography variant="subtitle2">{review.title}</Typography>
-                    <Typography variant="subtitle1">
-                        {review.author.email}
+        <>
+            <Grid item component={Paper} elevation={0}>
+                <Grid container alignItems="center">
+                    <div>
+                        <Typography variant="subtitle2">
+                            {review.title}
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            {review.author.email}
+                        </Typography>
+                    </div>
+                    <div className={classes.grow} />
+                    <Rating value={review.rating} readOnly />
+                    <PrivateComponent adminOnly>
+                        <Tooltip title="Delete review">
+                            <IconButton size="small" onClick={openDialog}>
+                                <DeleteForeverIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </PrivateComponent>
+                </Grid>
+                <Collapse
+                    collapsedHeight="90px"
+                    in={expanded}
+                    className={classes.padding}
+                >
+                    <Typography variant="body2" align="justify">
+                        {review.content}
                     </Typography>
-                </div>
-                <div className={classes.grow} />
-                <Rating value={review.rating} readOnly />
-                <PrivateComponent adminOnly>
-                    <Tooltip title="Delete review">
-                        <IconButton size="small">
-                            <DeleteForeverIcon />
-                        </IconButton>
-                    </Tooltip>
-                </PrivateComponent>
+                </Collapse>
+                <Button
+                    onClick={() => setExpanded(!expanded)}
+                    variant="outlined"
+                    className={classes.button}
+                >
+                    {expanded ? "Read less" : "Read more"}
+                </Button>
+                <Divider variant="middle" />
             </Grid>
-            <Collapse
-                collapsedHeight="90px"
-                in={expanded}
-                className={classes.padding}
-            >
-                <Typography variant="body2" align="justify">
-                    {review.content}
-                </Typography>
-            </Collapse>
-            <Button
-                onClick={() => setExpanded(!expanded)}
-                variant="outlined"
-                className={classes.button}
-            >
-                {expanded ? "Read less" : "Read more"}
-            </Button>
-            <Divider variant="middle" />
-        </Grid>
+            <Dialog />
+        </>
     );
 };
 
