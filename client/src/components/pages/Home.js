420ad541b1a94cb54db54c2fe5fe8fc85b221b4b
Faithfinder <faithfinder@yandex.ru>
2019-12-01 17:11:17 -0500

    Extract categories

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,14 +2,15 @@ import React from "react";
 
 import FeaturedItems from "src/components/items/FeaturedItems";
 import MainPageCategory from "src/components/items/MainPageCategory";
+import categories from "src/types/categories";
 
 export default () => {
     return (
         <>
             <FeaturedItems />
-            <MainPageCategory category="Music" />
-            <MainPageCategory category="Video" />
-            <MainPageCategory category="Games" />
+            {categories.map(category => (
+                <MainPageCategory key={category} category={category} />
+            ))}
         </>
     );
 };
