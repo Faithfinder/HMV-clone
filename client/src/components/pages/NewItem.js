5c9dab0cfc6e45749f485d3074ea0bab119a29e7
Faithfinder <faithfinder@yandex.ru>
2019-12-17 15:55:21 -0500

    Item creation page

--- client/src/components/pages/NewItem.js
+++ client/src/components/pages/NewItem.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+import Typography from "@material-ui/core/Typography";
+
+import useQuery from "src/util/useQuery";
+import ItemForm from "src/components/items/ItemForm";
+import Item from "src/types/Item";
+
 const NewItem = () => {
-    return <div>I am item creation!</div>;
+    const { category } = useQuery();
+
+    const postItem = formData => {
+        console.log(formData);
+    };
+
+    return (
+        <>
+            <Typography variant="h2">Create a new item</Typography>
+            <ItemForm
+                submitAction={postItem}
+                initialItem={new Item(category)}
+            />
+        </>
+    );
 };
 
 export default NewItem;
